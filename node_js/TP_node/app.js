6ad86ee7a54d19c2c0b4d319449e41e8e97619ba
Vincent Beauvieux <vbeauvieux@gmail.com>
2015-11-06 14:19:42 +0100

    updated loadpres route.

--- node_js/TP_node/app.js
+++ node_js/TP_node/app.js
@@ -3,12 +3,13 @@
 var CONFIG = require("./config.json");
 process.env.CONFIG = JSON.stringify(CONFIG);
 
-var module1 = require('app/utils.js');
+var module1 = require('./app/utils/utils.js');
 
 //Server configuration
 var express = require("express");
 var http = require("http");
 var defaultRoute = require("./app/routes/default.route.js");
+var loadPresRoute = require("./app/routes/loadPres.route.js");
 var path = require("path");
 
 //Init server
@@ -20,9 +21,4 @@ server.listen(CONFIG.port);
 app.use("/",defaultRoute);
 app.use("/admin", express.static(path.join(__dirname, "public/admin")));
 app.use("/watch", express.static(path.join(__dirname, "public/watch")));
-
-app.use("/loadPres", function() {
-	
-
-});
-app.use("/")
+app.use("/loadPres", loadPresRoute);
